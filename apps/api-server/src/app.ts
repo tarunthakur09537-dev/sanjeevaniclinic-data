b6f30462a86f706cd9cd94db3e12732266fb6259
sanjeevani-enetry <tarunthakur09537@gmail.com>
2026-03-25 16:46:48 +0530

    fix: remove pino-http middleware to fix TS2349 on Vercel serverless

--- apps/api-server/src/app.ts
+++ apps/api-server/src/app.ts
@@ -1,31 +1,9 @@
-import express, { type Express, Request, Response } from "express";
+import express, { type Express } from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
 import router from "./routes/index.js";
-import { logger } from "./lib/logger.js";
 
 const app: Express = express();
 
-app.use(
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  (pinoHttp as any)({
-    logger,
-    serializers: {
-      req(req: Request) {
-        return {
-          id: req.id,
-          method: req.method,
-          url: req.url?.split("?")[0],
-        };
-      },
-      res(res: Response) {
-        return {
-          statusCode: res.statusCode,
-        };
-      },
-    },
-  }),
-);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
